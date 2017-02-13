d6ecb2002b844a79ef93e0ec0c3b46141686889a
assafhp <assafshp@zahav.net.il>
2017-02-13 14:17:01 +0200

    func1

--- func-tmp-1/index.js
+++ func-tmp-1/index.js
@@ -1,1 +1,13 @@
-index.js
+var MonitorManager = require( '../monitorManager.js');
+
+module.exports = function (azureContext) {
+
+    var timeStamp = new Date().toISOString();
+    var monitorManager = new MonitorManager();
+   
+    azureContext.log('func-tmp-1 was triggered !!!');
+    monitorManager.log('func-tmp-1',timeStamp, true);
+
+    azureContext.done();
+    
+};
