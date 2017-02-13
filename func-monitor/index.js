0a5eb8836d0d14a13a591e5c5c767ac713f407ca
assafhp <assafshp@zahav.net.il>
2017-02-13 17:15:23 +0200

    final

--- func-monitor/index.js
+++ func-monitor/index.js
@@ -1,8 +1,10 @@
+var MonitorManager = require('./monitorManager.js');
 var util = require( '../util.js');
 
 module.exports = function (azureContext, data) {
 	
     azureContext.log('Getting monitor data');
+    var monitorManager = MonitorManager();
     var monitorData = monitorManager.get();
     util.logFunctionExecution(azureContext, __dirname, true);
     azureContext.res = {
