611da8af1421850569a6b03716b4c559ba51180a
assafhp <assafshp@zahav.net.il>
2017-02-13 17:18:50 +0200

    fix path

--- func-monitor/index.js
+++ func-monitor/index.js
@@ -1,4 +1,4 @@
-var MonitorManager = require('./monitorManager.js');
+var MonitorManager = require('../monitorManager.js');
 var util = require( '../util.js');
 
 module.exports = function (azureContext, data) {
