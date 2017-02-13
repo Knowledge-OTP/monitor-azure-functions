de897b95a8478447d1bcae208dcf49699d263fea
assafhp <assafshp@zahav.net.il>
2017-02-13 17:02:49 +0200

    util

--- func-tmp-2/index.js
+++ func-tmp-2/index.js
@@ -2,8 +2,7 @@ var util = require( '../util.js');
 
 module.exports = function (azureContext) {
 
-	var Util = new util();
-    Util.logFunctionExecution(azureContext, __dirname, true);
+    util.logFunctionExecution(azureContext, __dirname, true);
 
     azureContext.done();
     
--- util.js
+++ util.js
@@ -1,16 +1,20 @@
-var MonitorManager = require('./monitorManager.js');
-const path = require('path');
+(function (process) {
 
-var util={};
+	var MonitorManager = require('./monitorManager.js');
+	const path = require('path');
 
-util.prototype.logFunctionExecution = function(azureContext, dirname, isSucess){
+	var logFunctionExecution = function(azureContext, dirname, isSucess){
 	
-	var timeStamp = new Date().getTime();
-    var monitorManager = MonitorManager();   
-    var functionName = path.basename(dirname);
+		var timeStamp = new Date().getTime();
+	    var monitorManager = MonitorManager();   
+	    var functionName = path.basename(dirname);
 
-    azureContext.log(functionName + ' was triggered !!!');
-    monitorManager.log(functionName,timeStamp, isSucess);
-}
+	    azureContext.log(functionName + ' was triggered !!!');
+	    monitorManager.log(functionName,timeStamp, isSucess);
+	}
 
-module.exports = util;
+	module.exports = {
+        logFunctionExecution : logFunctionExecution
+    };
+
+})(process);
