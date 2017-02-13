d087b7f5402b26eedc52be2637255c831f43826b
assafhp <assafshp@zahav.net.il>
2017-02-13 16:18:15 +0200

    added util

--- func-monitor/index.js
+++ func-monitor/index.js
@@ -1,42 +1,23 @@
 var MonitorManager = require( '../monitorManager.js');
 const path = require('path');
-// const TOKEN = 'abcdfdsfs334asfdgvsdkdjf@#4sdadf^523423432sdfl';
 
 module.exports = function (azureContext, data) {
 
     var timeStamp = new Date().toISOString();
-    var monitorManager = MonitorManager();
-   
-    azureContext.log('dir name=' + __dirname);
+    var monitorManager = MonitorManager();   
     var functionName = path.basename(__dirname);
-    azureContext.log('functionName=' + functionName);
+
     azureContext.log(functionName + ' was triggered !!!');
-    monitorManager.log('func-monitor',timeStamp, true);
-
-	// if('token' in data ) {
-		// azureContext.log('got token');
-        // if (token===TOKEN){
-           azureContext.log('Getting monitor data');
-            var msg = monitorManager.get();
-            azureContext.res = {
-                status: 200,
-                body: msg
-            }; 
-        // }
-        // else {
-        //     azureContext.res = {
-        //         status: 401,
-        //         body: { error: 'invalid token'}
-        //     };
-        // }
-    // }
-    // else {
-    //     azureContext.res = {
-    //         status: 401,
-    //         body: { error: 'invalid token'}
-    //     };
-    // }
-
+    
+	
+   azureContext.log('Getting monitor data');
+    var msg = monitorManager.get();
+    monitorManager.log(functionName,timeStamp, true);
+    azureContext.res = {
+        status: 200,
+        body: msg
+    }; 
+    
     azureContext.done();
     
 };
--- func-tmp-2/index.js
+++ func-tmp-2/index.js
@@ -1,12 +1,8 @@
-var MonitorManager = require( '../monitorManager.js');
+var util = require( '../util.js');
 
 module.exports = function (azureContext) {
 
-    var timeStamp = new Date().toISOString();
-    var monitorManager = new MonitorManager();
-   
-    azureContext.log('func-tmp-2 was triggered !!!');
-    monitorManager.log('func-tmp-2',timeStamp, true);
+    util.logFunctionExecution(azureContext, __dirname, true);
 
     azureContext.done();
     
