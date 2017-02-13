7bb3775ca6ddef37be475a03912bffdf7b050689
assafhp <assafshp@zahav.net.il>
2017-02-13 18:08:23 +0200

    fix

--- func-tmp-2/index.js
+++ func-tmp-2/index.js
@@ -1,4 +1,4 @@
-var AzureFuncMonitorManager = require('./azureFuncMonitorManager.js');
+var AzureFuncMonitorManager = require('../azureFuncMonitorManager.js');
 
 module.exports = function (azureContext) {
 
